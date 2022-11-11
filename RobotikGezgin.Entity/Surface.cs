75e7bc5b1c3b321373153722f43c10c41274aca6
Burak Tomakin <brk.tmkn@gmail.com>
2022-11-11 19:57:43 +0300

    first commit and project done

---
+++ RobotikGezgin.Entity/Surface.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotikGezgin.Entity
+{
+    public class Surface
+    {
+        public int XCoordinationMax;
+        public int YCoordinationMax;
+
+        public Surface(int xCoordinationMax, int yCoordinationMax)
+        {
+            XCoordinationMax = xCoordinationMax;
+            YCoordinationMax = yCoordinationMax;
+        }
+
+        public Surface()
+        {
+
+        }
+    }
+}
---
+++ RobotikGezgin/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RobotikGezgin.Entity;
+using RobotikGezgin.Service;
+
+var service = new RobotMoverService();
+
+// Get surface datas
+Console.WriteLine("Yüzey bilgilerini girin");
+string? surfaceInfos = Console.ReadLine();
+Surface surface = service.CreateSurface(surfaceInfos);
+
+// Get first robots infos
+Console.WriteLine("İlk robotun bilgilerini giriniz.");
+string? firstRobotInfos = Console.ReadLine();
+Robot firstRobot = service.CreateRobot(firstRobotInfos, surface);
+
+// Move/Turn first robot
+Console.WriteLine("İlk robotun hareketini giriniz.");
+string? firstRobotMovements = Console.ReadLine();
+service.MoveRobot(firstRobotMovements, firstRobot, surface);
+
+// Get second robot infos
+Console.WriteLine("İkinci robotun bilgilerini giriniz.");
+string? secondRobotInfos = Console.ReadLine();
+Robot secondRobot = service.CreateRobot(secondRobotInfos, surface);
+
+// Move/Turn second robot
+Console.WriteLine("İkinci robotun hareketini giriniz.");
+string? secondRobotMovements = Console.ReadLine();
+service.MoveRobot(secondRobotMovements, secondRobot, surface);
+
+// Output first robot status
+firstRobot.WriteStatus();
+
+// Output second robot status
+secondRobot.WriteStatus();
